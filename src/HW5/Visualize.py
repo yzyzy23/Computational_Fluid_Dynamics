23981c03db1b2b0987a5c57a08f00ca56ef66187
yzyzy23 <2100013223@stu.pku.edu.cn>
2025-05-22 21:06:28 +0800

    HW5 done.

--- src/HW5/Visualize.py
+++ src/HW5/Visualize.py
@@ -11,8 +11,8 @@ def read_data(data_name, h):
             i = int(line.split()[0])
             j = int(line.split()[1])
             data[j][i] = float(line.split()[2])
-    u = numpy.gradient(data, axis=0)
-    v = -numpy.gradient(data, axis=1)
+    u = numpy.gradient(data, h, axis=0)
+    v = -numpy.gradient(data, h, axis=1)
     return data, x, y, u, v
 
 def plot_contour(x, y, data, name=None):
@@ -58,6 +58,15 @@ def plot_midline_velocity(x, y, u, v):
     plt.savefig('./Computational_Fluid_Dynamics/photo/HW5/MidlineVelocityU.png')
     plt.close()
 
+def locate_vortices(data, h):
+    for i in range(1, len(data) - 1):
+        for j in range(1, len(data[0]) - 1):
+            if (data[i][j] < data[i-1][j] and
+                data[i][j] < data[i+1][j] and
+                data[i][j] < data[i][j-1] and
+                data[i][j] < data[i][j+1]):
+                print(f"Vortex located at ({j * h}, {i * h}) with stream function value {data[i][j]}")
+
 if __name__ == "__main__":
     h = 0.01
     data_name = "./Computational_Fluid_Dynamics/src/HW5/StreamFunction.txt"
@@ -65,6 +74,7 @@ if __name__ == "__main__":
     plot_contour(x, y, data, name="StreamFunction")
     plot_stream_line(x, y, u, v)
     plot_midline_velocity(x, y, u, v)
+    locate_vortices(data, h)
     data_name = "./Computational_Fluid_Dynamics/src/HW5/Vorticity.txt"
     data, x, y, _, _ = read_data(data_name, h)
     plot_contour(x, y, data, name="Vorticity")
